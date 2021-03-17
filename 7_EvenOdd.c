6cbedac6e9656c9a79abadf2fb72abf0fea35321
Yogita-T <2020a1r118@mietjammu.in>
2021-03-17 22:18:12 +0530

    Even Odd program

---
+++ 7_EvenOdd.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int n;
+	printf("Enter point number");
+	scanf("%d",&n);
+	if (n%2==0)
+	   printf("number is even %d",n);
+	else
+	     printf("number is odd %d",n);
+	return 0;
+}

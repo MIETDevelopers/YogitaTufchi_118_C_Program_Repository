9f1b88bddc0f10ab1bc7fd3035507385e7a8db64
Yogita-T <2020a1r118@mietjammu.in>
2021-03-18 21:15:04 +0530

    Added comments to Hello World

--- 3_Hello.c
+++ 3_Hello.c
@@ -1,6 +1,9 @@
-#include <stdio.h>
+//Name: Yogita Tufchi
+//Program:Hello World Program
 
-int main(){
-  printf("Hello World");
-  return 0;
+#include <stdio.h> //Pre-process directive to include standard input and output functions header file
+
+int main(){      //Main Function
+  printf("Hello World\n");   //Printf function calling (to print hello world)
+  return 0; //return function
 }

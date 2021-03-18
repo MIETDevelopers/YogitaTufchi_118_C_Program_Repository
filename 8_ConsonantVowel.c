7c34420f77eb66f5ae72f50f484335613a8ac343
Yogita-T <2020a1r118@mietjammu.in>
2021-03-18 22:05:12 +0530

    Added comments for Consonant or Vowels

--- 8_ConsonantVowel.c
+++ 8_ConsonantVowel.c
@@ -1,10 +1,13 @@
-#include <stdio.h>
+//Yogita Tufchi
+//Program: Even or odd alphabets
+#include <stdio.h>    //Pre-process directive to include standard input and output functions header file
 
-int main()
+int main()            //main function
 {
-	char ch;
-	printf("Enter character\n");
-	scanf("%c",&ch);
+	char ch;      //variable declaration with data type char
+	printf("Enter character\n");  //printf function calling
+	scanf("%c",&ch);        //scanf function calling
+	//variable definition using switch case for both uppercase and lowercase variable "c"
 	switch(ch)
 	{
 	    case'a':
@@ -19,9 +22,10 @@ int main()
 	    case'U':
 	    printf("%c is a vowel",ch);
 	    break;
+	    //if variable doesn't match any case variable 
 	    default:
 	    printf("%c is a consonant",ch);
 	    break;
 	}   
-	 return 0;
+	 return 0; //return function 
 }

430d18997a841a3c0f2cd7aba3f9b94b7338fb14
Yogita-T <2020a1r118@mietjammu.in>
2021-03-19 21:48:59 +0530

    Check number is Armstrong

---
+++ 15_Armstrong.c
@@ -0,0 +1,21 @@
+#include<stdio.h>          // include stdio.h library   // variable declaration
+
+int main()                 //Main function
+{
+int n,num,r,ans=0;    // variable declaration
+printf("Enter a positive integer: ");    //Ask user to input positive number  
+scanf("%d", &num);
+n=num;
+while(n>0)                 /* Loop to calculate the sum of the cubes of its digits */
+{
+r=n%10;
+ans=ans+r*r*r;
+n=n/10;
+}
+ 
+if(ans==num)               /* if else condition to print Armstrong or Not */
+printf("%d is an Armstrong number.",num);
+else
+printf("%d is not an Armstrong number.",num);
+return 0;   // return 0 to oprerating system
+}

34dddd9b59babba8986634041cdcde4d5fae8359
Yogita-T <2020a1r118@mietjammu.in>
2021-03-20 22:01:49 +0530

    Read and Print elements

---
+++ 18_Dimensional.c
@@ -0,0 +1,28 @@
+//Name: Yogita Tufchi
+//Program: to read and print elements in two dimensional array. 
+
+#include<stdio.h>     //Pre-process directive to include standard input and output functions header file
+int main(){        //Main function
+   // 2D array declaration
+   int disp[2][3];
+   //Counter variables for the loop
+   int i, j;
+   for(i=0; i<2; i++) {
+      for(j=0;j<3;j++) {
+    //printf and scanf function calling    
+         printf("Enter value for disp[%d][%d]:\n", i, j);
+         scanf("%d", &disp[i][j]);
+      }
+   }
+   //Displaying array elements
+   printf("Two Dimensional array elements:\n");
+   for(i=0; i<2; i++) {
+      for(j=0;j<3;j++) {
+         printf("%d ", disp[i][j]);
+         if(j==2){
+            printf("\n");
+         }
+      }
+   }
+   return 0;   //Return Function
+} 

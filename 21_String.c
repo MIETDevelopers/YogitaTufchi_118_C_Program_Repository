f8aff8eccdf5ab8611a63a8d19112c404a2f6fd4
Yogita-T <2020a1r118@mietjammu.in>
2021-03-22 13:46:56 +0530

    Use of string operations

---
+++ 21_String.c
@@ -0,0 +1,20 @@
+//To demonstrate the use of various string operations
+//Name :Yogita Tufchi
+
+#include <stdio.h> //Pre-Process directive to include standard input output header file
+#include <string.h> //Pre-Process directive to include string input output header file
+int main()
+{
+    /* String Declaration*/
+    char nickname[20];
+
+    /* Console display using puts */
+    puts("Enter your Nick name:");
+
+    /*Input using gets*/
+    gets(nickname);
+
+    puts(nickname);
+
+    return 0;
+}

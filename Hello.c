60ac4a8c7db9055fc75c6686a235cea0f7fcfc34
Yogita-T <2020a1r118@mietjammu.in>
2021-03-28 18:09:10 +0530

    Assignment Q4

---
+++ Hello.c
@@ -0,0 +1,9 @@
+//Name: Yogita Tufchi
+//Program:Hello World Program
+
+#include <stdio.h> //Pre-process directive to include standard input and output functions header file
+
+int main(){      //Main Function
+  printf("Hello World\n");   //Printf function calling (to print hello world)
+  return 0; //return function
+}

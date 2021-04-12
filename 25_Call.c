998158b8cdabaa951f29709a2e66122d65eff219
Yogita-T <2020a1r118@mietjammu.in>
2021-04-12 17:07:59 +0530

    Swap by value

--- 25_Call.c
+++ 25_Call.c
@@ -1,20 +1,17 @@
- //Name: Yogita Tufchi
-//Program: Swap two numbers using call by value. 
-        
-#include <stdio.h> //Pre-process directive to include standard input and output functions header file
-  
-int main() {        //main function
-    int x, y; 
-    printf("Enter Value of x "); 
-    scanf("%d", &x); 
-    printf("\nEnter Value of y "); 
-    scanf("%d", &y); 
-    
-    //Here swapping of number occurs.
-    int temp = x; 
-    x = y; 
-    y = temp; 
-  
-    printf("\nAfter Swapping: x = %d, y = %d", x, y); 
-    return 0;   //return function
+//A C Program to swap two numbers using call by value.            
+#include<stdio.h>
+void swap(int, int);
+int main(){
+    int a, b;
+    printf("Enter two values: ");
+    scanf("%d %d", &a, &b);//Taking input from user.
+    swap(a,b);
+}
+//Here swapping exists.
+void swap (int a, int b){
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+    printf("%d %d", a, b);
 }
